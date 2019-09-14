2927a860db213232e60ed1edd5725e4cd7dc347f
Guillem Nieto <gnieto.talo@gmail.com>
2019-09-14 20:50:22 +0200

    migrate Overlapping Probes

--- src/linters/lints/overlapping_probes.rs
+++ src/linters/lints/overlapping_probes.rs
@@ -1,8 +1,9 @@
-use crate::linters::{Group, Lint, LintSpec};
+use crate::linters::{Group, KubeObjectType, Lint, LintSpec};
 
 use crate::reporting::Finding;
-use k8s_openapi::api::core::v1::{Container, PodSpec, PodStatus, Probe};
-use kube::api::{Object, ObjectMeta};
+use crate::visitor::{pod_spec_visit, PodSpecVisitor};
+use k8s_openapi::api::core::v1::{Container, PodSpec, Probe};
+use kube::api::ObjectMeta;
 use std::time::Duration;
 
 /// **What it does:** Finds pods which liveness probe *may* execute before all readiness probes has
@@ -24,22 +25,44 @@ use std::time::Duration;
 pub(crate) struct OverlappingProbes;
 
 impl Lint for OverlappingProbes {
-    fn v1_pod(&self, pod: &Object<PodSpec, PodStatus>) -> Vec<Finding> {
-        let mut findings = Vec::new();
+    fn object(&self, object: &KubeObjectType) -> Vec<Finding> {
+        let mut visitor = OverlappingProbesVisitor::default();
+        pod_spec_visit(&object, &mut visitor);
 
-        for c in pod.spec.containers.iter() {
-            self.check_container_probes(&mut findings, &c, &pod.metadata);
-        }
-
-        findings
+        visitor.findings
     }
 }
 
-impl OverlappingProbes {
-    fn spec() -> LintSpec {
-        LintSpec {
-            group: Group::Configuration,
-            name: "overlapping_probes".to_string(),
+#[derive(Default)]
+struct OverlappingProbesVisitor {
+    findings: Vec<Finding>,
+}
+
+impl PodSpecVisitor for OverlappingProbesVisitor {
+    fn visit_pod_spec(&mut self, pod_spec: &PodSpec, meta: &ObjectMeta) {
+        for c in pod_spec.containers.iter() {
+            self.check_container_probes(&c, &meta);
+        }
+    }
+}
+
+impl OverlappingProbesVisitor {
+    fn check_container_probes(&mut self, c: &Container, object_meta: &ObjectMeta) {
+        let readiness_probe = c.readiness_probe.as_ref().map(Self::calculate_time_frame);
+        let liveness_probes = c.liveness_probe.as_ref().map(Self::calculate_time_frame);
+
+        if let (Some(readiness), Some(liveness)) = (readiness_probe, liveness_probes) {
+            if readiness.overlaps_with(&liveness) {
+                let readiness_end = format!("{:?}", readiness.end);
+                let liveness_start = format!("{:?}", liveness.start);
+
+                let finding = Finding::new(OverlappingProbes::spec(), object_meta.clone())
+                    .add_metadata("container".to_string(), c.name.clone())
+                    .add_metadata("readiness_max_delay".to_string(), readiness_end)
+                    .add_metadata("liveness_start".to_string(), liveness_start);
+
+                self.findings.push(finding);
+            }
         }
     }
 
@@ -62,28 +85,13 @@ impl OverlappingProbes {
             end: max_duration,
         }
     }
+}
 
-    fn check_container_probes(
-        &self,
-        findings: &mut Vec<Finding>,
-        c: &Container,
-        object_meta: &ObjectMeta,
-    ) {
-        let readiness_probe = c.readiness_probe.as_ref().map(Self::calculate_time_frame);
-        let liveness_probes = c.liveness_probe.as_ref().map(Self::calculate_time_frame);
-
-        if let (Some(readiness), Some(liveness)) = (readiness_probe, liveness_probes) {
-            if readiness.overlaps_with(&liveness) {
-                let readiness_end = format!("{:?}", readiness.end);
-                let liveness_start = format!("{:?}", liveness.start);
-
-                let finding = Finding::new(Self::spec(), object_meta.clone())
-                    .add_metadata("container".to_string(), c.name.clone())
-                    .add_metadata("readiness_max_delay".to_string(), readiness_end)
-                    .add_metadata("liveness_start".to_string(), liveness_start);
-
-                findings.push(finding);
-            }
+impl OverlappingProbes {
+    fn spec() -> LintSpec {
+        LintSpec {
+            group: Group::Configuration,
+            name: "overlapping_probes".to_string(),
         }
     }
 }
@@ -96,5 +104,21 @@ struct TimeFrame {
 impl TimeFrame {
     pub fn overlaps_with(&self, frame: &TimeFrame) -> bool {
         self.end > frame.start
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::linters::lints::overlapping_probes::OverlappingProbes;
+    use crate::tests::{analyze_file, filter_findings_by};
+    use std::path::Path;
+
+    #[test]
+    fn it_finds_never_restart_errors() {
+        let findings = analyze_file(Path::new("tests/overlapping_probes.yaml"));
+        let findings = filter_findings_by(findings, &OverlappingProbes::spec());
+
+        assert_eq!(1, findings.len());
+        assert_eq!(findings[0].object_metadata().name, "hello-node");
     }
 }
